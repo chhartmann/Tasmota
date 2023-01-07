7de8a54ad6b44b4c221315445db637c6425ff58c
chhartmann <chhartmann@web.de>
2023-01-07 07:42:39 +0000

    integration of extended vito wifi

--- tasmota/tasmota_xsns_sensor/xsns_98_vito.ino
+++ tasmota/tasmota_xsns_sensor/xsns_98_vito.ino
@@ -5,6 +5,11 @@
 
 #define XSNS_98              98
 
+// TODOs:
+// - Add visualisation in web UI of values defined in a list
+// - Add button and function for "Warmwasser"
+// - Add button for "Set system time to current time"
+
 VitoWiFi_setProtocol(P300);
 
 DPTemp VitoTempAussen("Aussentemperatur", "temperatures", 0x0800);
@@ -12,46 +17,44 @@ DPTemp VitoTempSpeicher("Wassertemperatur", "temperatures", 0x0812);
 DPTemp VitoTempVLSoll("Soll-Vorlauftemperatur", "temperatures", 0x2544);
 DPTemp VitoTempRLIst("Ist-Ruecklauftemperatur", "temperatures", 0x0808);
 DPTemp VitoTempKesselSoll("Soll-Kesseltemperatur", "temperatures", 0x555A);
-DPCount VitoStartsBrenner("pump", "burner", 0x088A);
-DPCount VitoLaufzeitBrenner("pump", "burner", 0x08A7); // DPHours?
+DPUnsignedInt VitoStartsBrenner("pump", "burner", 0x088A);
+DPHours VitoLaufzeitBrenner("pump", "burner", 0x08A7);
 
-DPStat VitoStatusStoerung("Status", "status", 0x08A2);
+DPByte VitoStatusStoerung("Status", "status", 0x08A2);
+DPTimeStamp VitoSystemzeit("Systemzeit", "status", 0x088E);
 
-//TODO enum und zeit
-//DPxxx getVitoStoerung1("Stoerung-History1", "error-history", 0x7507)
-//DPxxx getVitoStoerung2("Stoerung-History2", "error-history", 0x7510)
-//DPxxx getVitoStoerung3("Stoerung-History3", "error-history", 0x7519)
-//DPxxx getVitoStoerung4("Stoerung-History4", "error-history", 0x7522)
-//DPxxx getVitoStoerung5("Stoerung-History5", "error-history", 0x752B)
-//DPxxx getVitoStoerung6("Stoerung-History6", "error-history", 0x7534)
-//DPxxx getVitoStoerung7("Stoerung-History7", "error-history", 0x753D)
-//DPxxx getVitoStoerung8("Stoerung-History8", "error-history", 0x7546)
-//DPxxx getVitoStoerung9("Stoerung-History9", "error-history", 0x754F)
-//DPxxx getVitoStoerung10("Stoerung-History10", "error-history", 0x7558)
+DPErrHist getVitoStoerung1("Stoerung-History1", "error-history", 0x7507);
+DPErrHist getVitoStoerung2("Stoerung-History2", "error-history", 0x7510);
+DPErrHist getVitoStoerung3("Stoerung-History3", "error-history", 0x7519);
+DPErrHist getVitoStoerung4("Stoerung-History4", "error-history", 0x7522);
+DPErrHist getVitoStoerung5("Stoerung-History5", "error-history", 0x752B);
+DPErrHist getVitoStoerung6("Stoerung-History6", "error-history", 0x7534);
+DPErrHist getVitoStoerung7("Stoerung-History7", "error-history", 0x753D);
+DPErrHist getVitoStoerung8("Stoerung-History8", "error-history", 0x7546);
+DPErrHist getVitoStoerung9("Stoerung-History9", "error-history", 0x754F);
+DPErrHist getVitoStoerung10("Stoerung-History10", "error-history", 0x7558);
 
-//TODO getVitoSystemzeit 088E len 8
+DPCycleTime VitoTimerMoHeizen("Heizen-Mo", "timer-heating", 0x2000);
+DPCycleTime VitoTimerDiHeizen("Heizen-Di", "timer-heating", 0x2008);
+DPCycleTime VitoTimerMiHeizen("Heizen-Mi", "timer-heating", 0x2010);
+DPCycleTime VitoTimerDoHeizen("Heizen-Do", "timer-heating", 0x2008);
+DPCycleTime VitoTimerFrHeizen("Heizen-Fr", "timer-heating", 0x2020);
+DPCycleTime VitoTimerSaHeizen("Heizen-Sa", "timer-heating", 0x2008);
+DPCycleTime VitoTimerSoHeizen("Heizen-So", "timer-heating", 0x2030);
 
-DPTimer VitoTimerMoHeizen("Heizen-Mo", "timer-heating", 0x2000);
-DPTimer VitoTimerDiHeizen("Heizen-Di", "timer-heating", 0x2008);
-DPTimer VitoTimerMiHeizen("Heizen-Mi", "timer-heating", 0x2010);
-DPTimer VitoTimerDoHeizen("Heizen-Do", "timer-heating", 0x2008);
-DPTimer VitoTimerFrHeizen("Heizen-Fr", "timer-heating", 0x2020);
-DPTimer VitoTimerSaHeizen("Heizen-Sa", "timer-heating", 0x2008);
-DPTimer VitoTimerSoHeizen("Heizen-So", "timer-heating", 0x2030);
-
-DPTimer VitoTimerDiWW("Warmwasser-Di", "timer-water", 0x2108);
-DPTimer VitoTimerMiWW("Warmwasser-Mi", "timer-water", 0x2110);
-DPTimer VitoTimerMoWW("Warmwasser-Mo", "timer-water", 0x2100);
-DPTimer VitoTimerDoWW("Warmwasser-Do", "timer-water", 0x2108);
-DPTimer VitoTimerFrWW("Warmwasser-Fr", "timer-water", 0x2120);
-DPTimer VitoTimerSaWW("Warmwasser-Sa", "timer-water", 0x2108);
-DPTimer VitoTimerSoWW("Warmwasser-So", "timer-water", 0x2130);
-
+DPCycleTime VitoTimerDiWW("Warmwasser-Di", "timer-water", 0x2108);
+DPCycleTime VitoTimerMiWW("Warmwasser-Mi", "timer-water", 0x2110);
+DPCycleTime VitoTimerMoWW("Warmwasser-Mo", "timer-water", 0x2100);
+DPCycleTime VitoTimerDoWW("Warmwasser-Do", "timer-water", 0x2108);
+DPCycleTime VitoTimerFrWW("Warmwasser-Fr", "timer-water", 0x2120);
+DPCycleTime VitoTimerSaWW("Warmwasser-Sa", "timer-water", 0x2108);
+DPCycleTime VitoTimerSoWW("Warmwasser-So", "timer-water", 0x2130);
 
 class LogPrinter : public Print {
   std::string line;
   void print_on_newline() {
     if (line.back() == '\n') {
+      line.pop_back();
       AddLog(LOG_LEVEL_INFO, "VTO: %s", (const char*)line.c_str());
       line.clear();
     }
@@ -62,7 +65,7 @@ class LogPrinter : public Print {
     return 1;
   };
   virtual size_t write(const uint8_t *buffer, size_t size) {
-    line += (char*)buffer;
+    line.append((char*)buffer, size);
     print_on_newline();
     return size;
   };
@@ -92,7 +95,7 @@ const char VitoCommandsString[] PROGMEM = "Vito|Read|LogOn|LogOff";
 void (* const VitoCommandsList[])(void) PROGMEM = { &VitoCommandRead, &VitoCommandLogOn, &VitoCommandLogOff };
 
 void globalCallbackHandler(const class IDatapoint& dp, class DPValue value) {
-  char value_str[15] = {0};
+  char value_str[30] = {0};
   value.getString(value_str, sizeof(value_str));
   AddLog(LOG_LEVEL_INFO, "VTO: %s %s %s", dp.getGroup(), dp.getName(), value_str);
 }
@@ -111,7 +114,7 @@ bool Xsns98(uint8_t function) {
     case FUNC_INIT:
       ClaimSerial();
       VitoWiFi.setLogger(&logPrinter);
-      VitoWiFi.enableLogger();
+//      VitoWiFi.enableLogger();
       VitoWiFi.setup(&Serial);
       VitoWiFi.setGlobalCallback(globalCallbackHandler);
       break;
